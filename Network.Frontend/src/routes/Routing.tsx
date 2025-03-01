8bdc4dbb7d639ff5e2a3f1774fc7c1962f520838
PiotrZak <37672365+PiotrZak@users.noreply.github.com>
2025-03-01 20:31:01 +0100

    hash router

--- Network.Frontend/src/routes/Routing.tsx
+++ Network.Frontend/src/routes/Routing.tsx
@@ -2,18 +2,20 @@ import Contact from "@/pages/Contact";
 import Home from "@/pages/Home";
 import London from "@/pages/London";
 import Agents from "@/pages/Agents";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const Routing: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="london" element={<London />} />
-      <Route path="agents" element={<Agents />} />
-      {/* <Route path="*" element={<PageNotFound />} /> */}
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="london" element={<London />} />
+        <Route path="agents" element={<Agents />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
+      </Routes>
+    </HashRouter>
   );
 };
 
-export default Routing;
+export default Routing; x
