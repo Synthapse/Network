e5f5db1d204513a97868a00f8c14ec7ad1465670
Muhammad Qasim <qasimyousaf094@gmail.com>
2025-03-01 07:16:04 +0500

    update the code of the app

---
+++ Network.NextFrontend/my-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import HomePage from "./home/page";
+
+export default function Home() {
+  return (
+<div className="flex justify-center items-center min-h-screen">
+  <HomePage />
+</div>
+
+  );
+}
